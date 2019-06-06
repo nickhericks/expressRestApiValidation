91e21d4aaf0e9465b4ad828316e767b32bcb18b1
Nick-Hericks <nickhericks@gmail.com>
2019-06-06 17:19:17 +0200

    Fix formatting

--- routes.js
+++ routes.js
@@ -16,14 +16,22 @@ router.get('/users', (req, res) => {
 
 // Route that creates a new user.
 router.post('/users', (req, res) => {
+  console.log('POST request sent.');
+
   // Get the user from the request body.
   const user = req.body;
+
+  console.log('req.body added to user variable.');
 
   // Add the user to the `users` array.
   users.push(user);
 
+  console.log('user data pushed to users array.');
+
   // Set the status to 201 Created and end the response.
   res.status(201).end();
+
+  console.log('201 status code sent.');
 });
 
 module.exports = router;
