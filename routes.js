139fd2296153362afc099bd27037d227f8ca00e3
Nick-Hericks <nickhericks@gmail.com>
2019-06-06 19:16:11 +0200

    Add custom passwordConfirmation validation

--- routes.js
+++ routes.js
@@ -18,48 +18,69 @@ router.get('/users', (req, res) => {
 
 
 // Route that creates a new user.
-router.post('/users', [
-  check('name')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "name"'),
-  check('email')
-    .exists({ checkNull: true, checkFalsy: true })
-		.withMessage('Please provide a value for "email"')
-		.isEmail()
-		.withMessage('Please provide a valid email address for "email"'),
-  check('birthday')
-    .exists({ checkNull: true, checkFalsy: true })
-		.withMessage('Please provide a value for "birthday"')
-		.isDate()
-		.withMessage(),
-  check('password')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "password"'),
-  check('passwordConf')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "passwordConf"'),
-], (req, res) => {
-  // Attempt to get the validation result from the Request object.
-  const errors = validationResult(req);
+router.post(
+  '/users',
+  [
+    check('name')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "name"'),
+    check('email')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "email"')
+      .isEmail()
+      .withMessage('Please provide a valid email address for "email"'),
+    check('birthday')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "birthday"')
+      .isISO8601()
+      .withMessage('Please provide a valid date for "birthday"'),
+    check('password')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "password"')
+      .isLength({ min: 8, max: 20 })
+      .withMessage(
+        'Please provide a value for "password" that is between 8 and 20 characters in length',
+      ),
+    check('passwordConfirmation')
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "passwordConfirmation"')
+      .custom((value, { req }) => {
+        // Only attempt to compare the `password` and `passwordConfirmation`
+        // fields if they have values.
+        if (value && req.body.password && value !== req.body.password) {
+          throw new Error(
+            'Please provide values for "password" and passwordConfirmation" that match',
+          );
+        }
 
-  // If there are validation errors...
-  if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
-    const errorMessages = errors.array().map(error => error.msg);
+        // Return `true` so the default "Invalid value" error message
+        // doesn't get returned
+        return true;
+      }),
+  ],
+  (req, res) => {
+    // Attempt to get the validation result from the Request object.
+    const errors = validationResult(req);
 
-    // Return the validation errors to the client.
-    res.status(400).json({ errors: errorMessages });
-  } else {
-    // Get the user from the request body.
-    const user = req.body;
+    // If there are validation errors...
+    if (!errors.isEmpty()) {
+      // Use the Array `map()` method to get a list of error messages.
+      const errorMessages = errors.array().map(error => error.msg);
 
-    // Add the user to the `users` array.
-    users.push(user);
+      // Return the validation errors to the client.
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      // Get the user from the request body.
+      const user = req.body;
 
-    // Set the status to 201 Created and end the response.
-    res.status(201).end();
-  }
-});
+      // Add the user to the `users` array.
+      users.push(user);
+
+      // Set the status to 201 Created and end the response.
+      res.status(201).end();
+    }
+  },
+);
 
 
 module.exports = router;
