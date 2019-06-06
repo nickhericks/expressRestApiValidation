dc5f610b36b5f11d4913eafc0e908fa72db2aba2
Nick-Hericks <nickhericks@gmail.com>
2019-06-06 19:00:36 +0200

    Add library validation for email

--- routes.js
+++ routes.js
@@ -17,13 +17,15 @@ router.get('/users', (req, res) => {
 });
 
 
-const nameValidationChain = check('name')
-  .exists({ checkNull: true, checkFalsy: true })
-  .withMessage('Please provide a value for "name"');
-
-
 // Route that creates a new user.
-router.post('/users', nameValidationChain, (req, res) => {
+router.post('/users', [
+  check('name')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "name"'),
+  check('email')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "email"'),
+], (req, res) => {
   // Attempt to get the validation result from the Request object.
   const errors = validationResult(req);
 
@@ -34,17 +36,16 @@ router.post('/users', nameValidationChain, (req, res) => {
 
     // Return the validation errors to the client.
     res.status(400).json({ errors: errorMessages });
+  } else {
+    // Get the user from the request body.
+    const user = req.body;
+
+    // Add the user to the `users` array.
+    users.push(user);
+
+    // Set the status to 201 Created and end the response.
+    res.status(201).end();
   }
-
-  // Get the user from the request body.
-  const user = req.body;
-
-
-  // Add the user to the `users` array.
-  users.push(user);
-
-  // Set the status to 201 Created and end the response.
-  res.status(201).end();
 });
 
 
